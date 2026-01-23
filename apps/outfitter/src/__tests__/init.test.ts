eb85e866b0e387fb92de36910688443ef2b6a780
Matt Galligan <matt.galligan@gmail.com>
2026-01-23 17:06:24 -0500

    test(outfitter): make init test non-interactive

--- apps/outfitter/src/__tests__/init.test.ts
+++ apps/outfitter/src/__tests__/init.test.ts
@@ -1,6 +1,25 @@
-import { test, expect } from "bun:test";
+import { afterEach, beforeEach, expect, test } from "bun:test";
 
-import { InitError, runInit } from "../index";
+import { InitError, runInit } from "../index.js";
+
+let originalIsTTY: boolean | undefined;
+
+beforeEach(() => {
+	originalIsTTY = process.stdout.isTTY;
+	Object.defineProperty(process.stdout, "isTTY", {
+		value: false,
+		writable: true,
+		configurable: true,
+	});
+});
+
+afterEach(() => {
+	Object.defineProperty(process.stdout, "isTTY", {
+		value: originalIsTTY,
+		writable: true,
+		configurable: true,
+	});
+});
 
 test("runInit returns a helpful error for missing templates", async () => {
 	const result = await runInit({
