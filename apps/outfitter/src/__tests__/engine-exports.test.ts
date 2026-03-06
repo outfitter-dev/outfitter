d1d2b17aeb96b6fc6cc876e03683e25ce7f4fdf7
Matt Galligan <matt.galligan@gmail.com>
2026-03-06 07:10:18 -0500

    test(outfitter): lock engine export surface

--- apps/outfitter/src/__tests__/engine-exports.test.ts
+++ apps/outfitter/src/__tests__/engine-exports.test.ts
@@ -3,10 +3,30 @@ import { describe, expect, test } from "bun:test";
 import * as engine from "../engine/index.js";
 
 describe("engine exports", () => {
-  test("exposes preset-native helpers without legacy template aliases", () => {
-    expect(engine.getPresetsBaseDir).toBeFunction();
-    expect(engine.copyPresetFiles).toBeFunction();
-    expect("getTemplatesDir" in engine).toBe(false);
-    expect("copyTemplateFiles" in engine).toBe(false);
+  test("exposes the current public engine runtime surface without legacy template aliases", () => {
+    expect(Object.keys(engine).toSorted()).toEqual([
+      "ScaffoldError",
+      "addBlocks",
+      "buildWorkspaceRootPackageJson",
+      "copyPresetFiles",
+      "deriveBinName",
+      "deriveProjectName",
+      "detectWorkspaceRoot",
+      "executePlan",
+      "getOutputFilename",
+      "getPresetsBaseDir",
+      "injectSharedConfig",
+      "isBinaryFile",
+      "isPathWithin",
+      "replacePlaceholders",
+      "resolveAuthor",
+      "resolvePackageName",
+      "resolveYear",
+      "rewriteLocalDependencies",
+      "sanitizePackageName",
+      "scaffoldWorkspaceRoot",
+      "validatePackageName",
+      "validateProjectDirectoryName",
+    ]);
   });
 });
